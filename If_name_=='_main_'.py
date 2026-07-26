4ec81005f41ad21c30ab70785ab938892166c145
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-07-26 19:59:33 +0545

    if_name_==

---
+++ If_name_=='_main_'.py
@@ -0,0 +1,12 @@
+# New if:
+ 
+# if__name__ == __main__:(this script can be imported or run standalone)
+#                         Functions and classes in the module can be resused
+#                         without the main block of code executing
+
+
+def main():
+    # Your program goes here
+    pass
+if__name__ == '__main__':
+   main()    
