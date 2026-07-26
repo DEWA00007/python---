387576c9a73f025dcd8c0c51f57b15ffc3a9f944
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-07-26 19:47:14 +0545

    Variable scope and Scope resoulition

---
+++ Day_14_scope.py
@@ -0,0 +1,10 @@
+# variable scope--> where a variable is visible and accessible
+# scope resoultion --> (LEGB) local -> Enclosed -> Global -> Built-in
+   # 1st check at local scope for the variable then enclosed then global
+   #   and at last built-in 
+
+
+
+
+# A functions can't see inside of another function , but they
+# can inside of their own function.
