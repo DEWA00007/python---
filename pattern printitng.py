9b71658a8e121d22582cf996d00e325e195d91dd
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-08-12 20:21:04 +0545

    Priting pattern

--- pattern printitng.py
+++ pattern printitng.py
@@ -2,3 +2,14 @@
 n= 5 
 for i in range(1,n+1):
     print(" "*(n-i)+"*" * (2*i-1))
+
+
+
+"""a = 7
+for i in range(1,a+1):
+    print(" " *(a-i)+ "-" * (2*i-1) )   
+
+
+b = 10
+for i in range (1,b+1):
+    print(" "*(b-i)+ "^" * (2*i-1))  """  
