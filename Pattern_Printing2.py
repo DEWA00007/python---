0aef790eba3b6ebacf71bcf024a7bacd7e00882a
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-08-12 20:41:55 +0545

    pattern-2

--- Pattern_Printing2.py
+++ Pattern_Printing2.py
@@ -1,5 +1,16 @@
 # Some other pattern printing
-
+ # inverted pyramid
 n = 7
 for i in range(1,n+1):
     print(" "*(i-1)+"-"*(2*(n-i)+1))
+print()
+
+# Diamond   :
+
+n=7
+for i in range(1,n+1):
+    print(" "*(n-i)+"^"*(2*i-1)) 
+
+for j in range(1,n+1):
+    print(" "*(j-1)+"^"*(2*(n-j)+1))
+  
