f26928cfdb28575063d08abe105b0ec963e346e7
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-08-13 21:54:41 +0545

    practising more

--- Practising2_qu.py
+++ Practising2_qu.py
@@ -15,3 +15,30 @@ if is_spam:
     print("Spam comment detected!")
 else:
     print("This comment is not spam.")
+
+
+# Write a Python function to remove a given word from a list and strip it at the same time
+
+def remove_word(words, word):
+    new_list = []
+
+    for item in words:
+        item = item.strip()
+
+        if item != word:
+            new_list.append(item)
+
+    return new_list
+
+
+# Creating a list
+words = [" apple ", "banana", " orange ", "apple", " mango "]
+
+# Asking the user which word to remove
+word = input("Enter the word to remove: ")
+
+# Calling the function
+result = remove_word(words, word)
+
+# Displaying the updated list
+print("Updated list:", result)
