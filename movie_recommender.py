d430fa7fc1f65cca0a5873d126f43905003e43b0
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-08-14 22:47:31 +0545

    Movie recommendation

--- movie_recommender.py
+++ movie_recommender.py
@@ -5,12 +5,25 @@ url = "https://raw.githubusercontent.com/erik-sytnyk/movies-list/master/db.json"
 
 response = requests.get(url)
 
-movies = response.json()
+data = response.json()
 
-movie = random.choice(movies["movies"])
+movies = data["movies"]
 
-print("🎬 Movie Recommendation")
-print()
-print("Title:", movie["title"])
-print("Year:", movie["year"])
-print("Genres:", movie["genres"])
+genre = input("Enter a genre: ")
+
+matching_movies = []
+
+for movie in movies:
+    if genre.lower() in [g.lower() for g in movie["genres"]]:
+        matching_movies.append(movie)
+
+if matching_movies:
+    movie = random.choice(matching_movies)
+
+    print("\n🎬 Movie Recommendation")
+    print("Title:", movie["title"])
+    print("Year:", movie["year"])
+    print("Genres:", movie["genres"])
+
+else:
+    print("Sorry, no movies found for that genre.")
