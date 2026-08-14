7cbc790911a4cb91527cd13de31bef4f0dec592d
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-08-14 22:51:12 +0545

    Movie recommendation

--- movie_recommender.py
+++ movie_recommender.py
@@ -4,26 +4,50 @@ import random
 url = "https://raw.githubusercontent.com/erik-sytnyk/movies-list/master/db.json"
 
 response = requests.get(url)
-
 data = response.json()
 
 movies = data["movies"]
 
-genre = input("Enter a genre: ")
+print("🎬 MOVIE RECOMMENDER")
+print()
+print("Choose a genre:")
+print("1. Action")
+print("2. Comedy")
+print("3. Drama")
+print("4. Horror")
+print("5. Romance")
+print("6. Sci-Fi")
 
-matching_movies = []
+choice = input("\nEnter your choice: ")
 
-for movie in movies:
-    if genre.lower() in [g.lower() for g in movie["genres"]]:
-        matching_movies.append(movie)
+genres = {
+    "1": "Action",
+    "2": "Comedy",
+    "3": "Drama",
+    "4": "Horror",
+    "5": "Romance",
+    "6": "Sci-Fi"
+}
 
-if matching_movies:
-    movie = random.choice(matching_movies)
+if choice in genres:
+    selected_genre = genres[choice]
 
-    print("\n🎬 Movie Recommendation")
-    print("Title:", movie["title"])
-    print("Year:", movie["year"])
-    print("Genres:", movie["genres"])
+    matching_movies = []
+
+    for movie in movies:
+        if selected_genre.lower() in [g.lower() for g in movie["genres"]]:
+            matching_movies.append(movie)
+
+    if matching_movies:
+        movie = random.choice(matching_movies)
+
+        print("\n🍿 Your recommendation:")
+        print("Title:", movie["title"])
+        print("Year:", movie["year"])
+        print("Genres:", movie["genres"])
+
+    else:
+        print("Sorry, no movies found.")
 
 else:
-    print("Sorry, no movies found for that genre.")
+    print("❌ Invalid choice.")
