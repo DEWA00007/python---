a7a034f12a57d42af156a7c07aecc5a697ecac96
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-09-04 22:08:33 +0545

    Learning turtle 01

--- turtle/lesson_1.py
+++ turtle/lesson_1.py
@@ -6,3 +6,14 @@ t.right(90)
 t.forward(100)
 turtle.done()"""
 
+# Making square shape:
+
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(4):
+    t.forward(150)
+    t.right(90)
+
+turtle.done()
