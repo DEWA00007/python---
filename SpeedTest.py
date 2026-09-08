c846e67c199ec86f86cf25f206719a33f58aa467
Dewashish Pokharel <dewashishpokharel7@gmail.com>
2026-09-08 21:35:56 +0545

    Speed test

---
+++ SpeedTest.py
@@ -0,0 +1,13 @@
+# Typing speed test 
+
+import time
+import random
+
+sentences = [
+    "Python is fun to learn",
+    "Practice makes you better at coding.",
+    "I am learning Python one project at a time.",
+    "Programming is fun when you build cool things.",
+    "Never stop learning new skills.",
+    "The quick brown fox jumps over the lazy dog."
+             ]
